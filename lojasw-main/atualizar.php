e0c5a4eef0f54c6cda7ac4f881cb3ff3f340c4b2
Adriano Bendazzoli <159480772+Adrianobendazzoli@users.noreply.github.com>
2025-09-02 08:44:42 -0300

    Add files via upload

---
+++ lojasw-main/atualizar.php
@@ -0,0 +1,25 @@
+<?php
+    require 'conexão.php';
+   
+    $id = $_GET['id'];
+    $nome_novo = $_POST['nome_novo'];
+    $preco_novo = $_POST['preco_novo'];
+    $quantidade_novo = $_POST['quantidade_novo'];
+   
+    
+    $sql = "UPDATE produtos SET nome = :nome_novo, preco = :preco_novo, quantidade = :quantidade_novo 
+    WHERE id = :id";
+    
+    $stmt = $pdo->prepare($sql);
+    
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':nome_novo', $nome_novo);
+    $stmt->bindParam(':preco_novo', $preco_novo);
+    $stmt->bindParam(':quantidade_novo', $quantidade_novo);
+    
+    if ($stmt->execute()) {
+        echo "<script>alert('Produto atualizado com sucesso!');</script>";
+    } else {
+        echo "<script>alert('Erro ao atualizar produto.');</script>";
+    }
+?>
